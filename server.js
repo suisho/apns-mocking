a36cc1d9fc961f9e803039da0e3a62e82ffac053
suisho <suisho000000@gmail.com>
2013-09-10 21:06:35 +0900

    add buffer check

--- server.js
+++ server.js
@@ -7,52 +7,62 @@
 var PORT = 3434
 var tls = require('tls')
 var fs = require('fs')
+var allData = [];
 
 var server = tls.createServer({
   pfx : fs.readFileSync('mock.p12'), // mock
   passphrase : 'mock',                    //mock
-} , function(cleartextStream) {
+} , function(socket) {
   console.log('server connected',
-              cleartextStream.authorized ? 'authorized' : 'unauthorized');
-  cleartextStream.setEncoding('utf8');
+              socket.authorized ? 'authorized' : 'unauthorized');
 
-  cleartextStream.on('data', function(data){
-    console.log(data)
+  socket.on('data', function(data){
+    allData.push(data)
   })
-  cleartextStream.on('end', function(){
-    //console.log("aa")
-    //cleartextStream.write(0)
+  socket.on('end', function(){
+    allData.forEach(function(data){
+      try{
+        var buffer = readBuffer(data)
+        console.log(buffer)
+        socket.write("0")
+      }catch(e){
+        console.log(e)
+        socket.write("1")
+      }
+    })
+    socket.end()
   })
 }).listen(PORT)
-*/
+
+// https://gist.github.com/rahuloak/4949381
 var readBuffer = function(data) {
- 
+  var buffer = {}
   var offset = 0;
-  
-  // Command byte 
-  var command = data.readUInt8(offset);
+  // Command byte
+  buffer.command = data.readUInt8(offset);
+
   offset++;
   
   // Handle enhanced mode
-  if( command == 1 ) {
+  if( buffer.command == 1 ) {
     // Identifier
-    var id = data.readUInt32BE(offset);
+    buffer.id = data.readUInt32BE(offset);
     offset += 4;
     
     // Expiration
-    var expiry = data.readUInt32BE(offset);
+    buffer.expiry = data.readUInt32BE(offset);
     offset += 4;
   }
   
   // Token length
-  var tokenLength = data.readUInt16BE(offset);
+  buffer.tokenLength = data.readUInt16BE(offset);
   offset += 2;
   
   // Token
-  var tempBuf = new Buffer(tokenLength);
-  data.copy(tempBuf, 0, offset, offset+tokenLength);
-  var token = tempBuf.toString();
-  offset += tokenLength;
+  var tempBuf = new Buffer(buffer.tokenLength);
+  data.copy(tempBuf, 0, offset, offset+buffer.tokenLength);
+  buffer.token = tempBuf.toString('hex');
+  offset += buffer.tokenLength;
   
   // Payload length
   var payloadLength = data.readUInt16BE(offset);
@@ -61,5 +71,6 @@ var readBuffer = function(data) {
   // Payload
   var pBuf = new Buffer(payloadLength);
   data.copy(pBuf, 0, offset, offset+payloadLength);
-  var payload = pBuf.toString();
- }
+  buffer.payload = pBuf.toString('utf8');
+  return buffer
+}
