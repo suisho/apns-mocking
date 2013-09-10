cfd23ca3a0b5ee0451094b2111ba216091132ced
suisho <suisho000000@gmail.com>
2013-09-10 19:46:53 +0900

    init client

---
+++ client/src/test/java/Foo.java
@@ -0,0 +1,15 @@
+import com.notnoop.apns.APNS;
+import com.notnoop.apns.ApnsService;
+import org.testng.annotations.Test;
+
+/**
+ * @author: suisho
+ * @created: 2013/09/06 12:02
+ */
+
+public class Foo {
+    @Test
+    public void test(){
+        Main.connect();
+    }
+}
